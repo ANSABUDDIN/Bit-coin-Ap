f166fbef6a76024de568efead249729e17dd0170
“[ANSABUDDIN] <[ansabuddin0346@mail.com]>
2022-06-01 13:00:16 +0500

    ..

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,32 @@
 <template>
   <div>
-    <div>
-
-    </div>
+    <div></div>
     <!-- <div class="container">
       <h1 class="text-success" v-for="list in lists" :key="list" >{{list.first_name}}</h1>
     </div> -->
     <div class="container text-center my-5">
-      <h1 class="my-5 text-warning">Axios Api</h1>
-      <div class="row mx-auto mt-5 fw-bold" v-for="item in list" :key="item">
+      <h1 class="my-5 fw-bolder heading">Bit-Coin Api With Axios</h1>
+      <div class="mx-auto mt-5 fw-bold" v-for="item in list" :key="item">
         <div class="col">
           <!-- <div>
          <img :src="item.IMAGEURL" width="50px" class="rounded-circle" alt="" />
         </div> -->
 
-          <div class="  ">
-            <div class="px-5 bg-warning bit-logo ">
+          <div class="">
+            <div class="px-5 bit-logo">
               {{ item.FROMSYMBOL }}
             </div>
-             <div  class="px-5 mt-3 fw-bolder d-flex justify-content-between" >
-              <h5 class="fw-bolder" >OPENDAY</h5>
+            <div class="px-5 mt-3 fw-bolder d-flex justify-content-between">
+              <h5 class="fw-bolder">OPENDAY</h5>
               {{ item.OPENDAY }}
             </div>
             <div class="px-5 fw-bolder mt-3 d-flex justify-content-between">
-              <h5 class="fw-bolder" >Curent Price of Bit</h5>
+              <h5 class="fw-bolder">Curent Price of Bit</h5>
               {{ item.PRICE }}
             </div>
             <div>
               <!-- {{item.VOLUME24HOUR}} -->
             </div>
-           
           </div>
         </div>
       </div>
@@ -77,12 +74,16 @@ export default {
 </script>
 
 <style scoped>
- .bit-logo{
-   display:flex;
-   justify-content: center;
-   /* width: 30px; */
-   font-size: 50px;
-   border-radius:50px ;
-   
- }
+.bit-logo {
+  display: flex;
+  justify-content: center;
+  /* width: 30px; */
+  font-size: 50px;
+  background: #41b883 !important;
+  border-radius: 50px;
+}
+.heading{
+  color: #41b883 !important;
+  font-size:40px !important;
+}
 </style>
