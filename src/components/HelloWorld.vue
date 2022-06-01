491c0902d826bc78ac5e9851c38669334248921e
“[ANSABUDDIN] <[ansabuddin0346@mail.com]>
2022-06-01 13:14:11 +0500

    55

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,9 @@
 <template>
   <div>
-    <div></div>
-    <!-- <div class="container">
-      <h1 class="text-success" v-for="list in lists" :key="list" >{{list.first_name}}</h1>
-    </div> -->
     <div class="container text-center my-5">
       <h1 class="my-5 fw-bolder heading">Bit-Coin Api With Axios</h1>
       <div class="mx-auto mt-5 fw-bold" v-for="item in list" :key="item">
-        <div class="col">
-          <!-- <div>
-         <img :src="item.IMAGEURL" width="50px" class="rounded-circle" alt="" />
-        </div> -->
-
+        <div class="">
           <div class="">
             <div class="px-5 bit-logo">
               {{ item.FROMSYMBOL }}
@@ -24,9 +16,6 @@
               <h5 class="fw-bolder">Curent Price of Bit-Coin</h5>
               {{ item.PRICE }}
             </div>
-            <div>
-              <!-- {{item.VOLUME24HOUR}} -->
-            </div>
           </div>
         </div>
       </div>
@@ -35,24 +24,6 @@
 </template>
 
 <script>
-// import axios from "axios";
-// export default {
-//   name: 'HelloWorld',
-//   props: {
-//     msg: String
-//   },
-//   data()
-//   {
-//     return {
-//       list:[],
-//     }
-//   },
-//   async mounted() {
-//     let result = await axios.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR.json");
-//     console.log("api data", result.data);
-//     this.list = result.data;
-//   },
-// };
 import axios from "axios";
 
 export default {
@@ -77,13 +48,12 @@ export default {
 .bit-logo {
   display: flex;
   justify-content: center;
-  /* width: 30px; */
   font-size: 50px;
   background: #41b883 !important;
   border-radius: 50px;
 }
-.heading{
+.heading {
   color: #41b883 !important;
-  font-size:40px !important;
+  font-size: 40px !important;
 }
 </style>
